eb12ea8d0a65ad8385ac13d5f34c8e1809f55ce5
coder <s.khvalinsky@gmail.com>
2016-05-20 21:13:54 +0300

    Added: menu hiding phone protection

---
+++ js/ajax-telephone.js
@@ -0,0 +1,17 @@
+/**
+ * Created by coder on 20.05.16.
+ */
+function displayPhone() {
+    if(window.XMLHttpRequest){
+        xmlhttp = new XMLHttpRequest();
+    }else{
+        xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+    }
+    xmlhttp.onreadystatechange = function () {
+        if(xmlhttp.readyState == 4 && xmlhttp.status == 200){
+            document.getElementById("ajax-telephone").innerHTML = xmlhttp.responseText;
+        }
+    }
+    xmlhttp.open("GET","telephone_display.php",true);
+    xmlhttp.send();
+}
---
+++ telephone_display.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: coder
+ * Date: 20.05.16
+ * Time: 20:58
+ */
+
+echo "+38 (093) 123-45-67";

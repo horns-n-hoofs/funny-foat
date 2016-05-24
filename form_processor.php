50c709bec6b39b1c56fdcef368db5ed451238f87
coder <s.khvalinsky@gmail.com>
2016-05-24 20:20:06 +0300

    first draft of form procession

--- form_processor.php
+++ form_processor.php
@@ -16,12 +16,19 @@ $email = input_validator($_POST["contactFF"]);
 $tel = input_validator($_POST["telFF"]);
 $message = input_validator($_POST["messageFF"]);
 
+//hard to fix locale
+setlocale(LC_ALL, 'en_US');
+date_default_timezone_set("Europe/Kiev");
+
 $to = "funny_foam@yahoo.com";
-$headers = "From: admin@ketzer.pp.ua \r\n"
-    . "Reply-To: " . $email . "\r\n"
+$headers = "MIME-Version: 1.0\r\n"
+    . "From: admin@ketzer.pp.ua \r\n"
+    . "Content-Type: text/plain; charset=UTF-8 \r\n"
+    . "Reply-To: =?utf-8?b?".base64_encode($name)."?= <".$email.">"."\r\n"
     . "X-Mailer: PHP/" . phpversion();
-$subject = "Order";
-$message = wordwrap($message."\r\nTel.: ".$tel."\r\nE-mail: ".$email,70,"\r\n");
+$subject = "=?utf-8?b?".base64_encode("Order from ".$name." - ".$email)."?=";
+$message = wordwrap($message."\r\nTel.: ".$tel."\r\nE-mail: ".$email."\r\nDate: ".strftime("%A, %d-%B-%G %T"),70,"\r\n");
+//$message = "=?utf-8?b?".base64_encode($message)."?=";
 
 if(mail($to, $subject, $message, $headers)){
     echo "success";

e1c0eba589e4bda008a2393c7d0cd767b800f103
coder <s.khvalinsky@gmail.com>
2016-05-30 21:04:43 +0300

    injection protection

--- index.php
+++ index.php
@@ -210,17 +210,5 @@
    <script type="text/javascript" src="js/slider_options.js"></script>
    <script type="text/javascript" src="js/slideloaded.js"></script>
    <script type="text/javascript" src="js/atooltip.jquery.js"></script>
-   <script type="text/javascript">
-	   jQuery(function($) {
-		   $.mask.definitions['~']='[+-]';
-		   $('#date').mask('99/99/9999',{placeholder:"mm/dd/yyyy"});
-		   $('#phone').mask('(999) 999-9999');
-		   $('#phoneext').mask("(999) 999-9999? x99999");
-		   $("#tin").mask("99-9999999");
-		   $("#ssn").mask("999-99-9999");
-		   $("#product").mask("a*-999-a999",{placeholder:" ",completed:function(){alert("You typed the following: "+this.val());}});
-		   $("#eyescript").mask("~9.99 ~9.99 999");
-	   });
-   </script>
   </body>
  </html> 
